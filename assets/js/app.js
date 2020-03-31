f5e9b7cbc598d754366109b543778fa70fb88a31
Cibelle Montor <you@example.com>
2020-03-31 14:28:12 -0700

    hiding and showing working on all questions

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 const rootDiv = document.querySelector(".root");
 const startButton = document.querySelector("#button-start");
 const divStart = document.getElementById("start");
-const divQuestion1 = document.querySelector(".question1");
+const questionsAsked = [];
 
 //Listeners
 startButton.addEventListener("click", setupGame);
@@ -13,24 +13,32 @@ function setupGame() {
   createQuestions();
 }
 
+//Set up the questions creating a DIV for each
 function createQuestions() {
   let id = 0;
+
   questions.forEach(item => {
     id++;
-    const divQuestion = document.createElement("div");
-    divQuestion.classList = "question" + id;
-    divQuestion.classList = "hide";
-
     const title = item.title;
     const choice = item.choices;
+
+    const divQuestion = document.createElement("div");
     const titleNode = document.createElement("h6");
+
     titleNode.innerHTML = title;
+
+    //add predefined classes
+    divQuestion.classList = "hide";
+    titleNode.classList = "title";
+
+    //Apend to the HTML
     divQuestion.appendChild(titleNode);
     rootDiv.appendChild(divQuestion);
 
     choice.forEach(i => {
       const choiceNode = document.createElement("button");
       choiceNode.innerHTML = i;
+      choiceNode.classList = "button-choice";
       divQuestion.append(choiceNode);
       return;
     });
@@ -38,6 +46,25 @@ function createQuestions() {
   gameStart();
 }
 
+//Generates a ramdom div that will be displayed
 function gameStart() {
-  divQuestion1.style.display = "block";
+  const allDiv = document.querySelectorAll("div.hide");
+  var randomDiv = allDiv[Math.floor(allDiv.length * Math.random())];
+  if (questionsAsked.includes(randomDiv)) {
+    return;
+  } else {
+    randomDiv.classList = "show";
+    questionsAsked.push(randomDiv);
+  }
+  rootDiv.addEventListener("click", e => {
+    const isButton = e.target.nodeName === "BUTTON";
+    if (!isButton) {
+      return;
+    } else randomDiv.classList = "hide";
+    gameStart();
+  });
+}
+
+function questionValidation() {
+  console.log("hi");
 }
