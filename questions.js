48cbd36d23a5efccebdc690c3826aed270caa68e
Cibelle Montor <you@example.com>
2020-03-30 17:58:04 -0700

    initial commit with questions and start page

---
+++ questions.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    title: "What was the city where COVID-19 was first identified?",
+    choices: ["Taiwan", "Wuhan", "Bangkok", "Brisbane", "Yokohama"],
+    answer: "Wuhan"
+  },
+  {
+    title: " ___ is NOT a symptom of COVID-19",
+    choices: ["Fever", "Cough", "Shortness of breath", "Hair loss", "Tirednes"],
+    answer: "Hair loss"
+  },
+  {
+    title:
+      "According to the World Health Organization you can help prevent spreading the virus to others if you : ",
+    choices: [
+      "Wash your hands regularly for 20 seconds, with soap and water",
+      "Cover your nose and mouth with a disposable tissue or flexed elbow when you cough or sneeze",
+      "Avoid close contact (1 meter or 3 feet) with people who are unwell",
+      "Stay home and self-isolate from others in the household if you feel unwell",
+      "All of above"
+    ],
+    answer: " All of above"
+  },
+  {
+    title: "What is the recovery time for the coronavirus disease?",
+    choices: [
+      "2 days",
+      "1 year",
+      "2-6 weeks depending on the severity",
+      "6 months",
+      "None"
+    ],
+    answer: " 2-6 weeks depending on the severity"
+  },
+  {
+    title: "Which is the FALSE statement?",
+    choices: [
+      "COVID-19 vaccine was developed in Feb 2020 by Researchers at Stanford University ",
+      "COVID-19 spreads primarily through droplets generated when an infected person coughs or sneezes",
+      " There is no evidence that pets such as dogs or cats can be infected with COVID-19",
+      " People of all ages can be infected by COVID-19",
+      "Staying home when sick i's a way to prevent spreading the COVID-19"
+    ],
+    answer:
+      "COVID-19 vaccine was developed in Feb 2020 by Researchers at Stanford University "
+  }
+];
+
+module.import = questions;
